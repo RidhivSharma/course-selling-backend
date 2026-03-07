ecf89ae9af2ffa2e112d25997e7005a2981e0c26
ridhivsharma <ridhiv0305@gmail.com>
2026-03-07 23:50:25 +0530

    database is connected and runnig very fine

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express=require("express");
+const express = require("express");
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const mongoose = require("mongoose");
+
 const { userRouter } = require("./routes/user");
 const { coursesRouter } = require("./routes/courses");
-const adminRouter = require("./routes/adminRouter");
-const app=express();
+const {adminRouter} = require("./routes/adminRouter");
+const app = express();
 
-
-app.use("/user",userRouter)
-app.use("/courses",coursesRouter)
+app.use("/user", userRouter);
+app.use("/courses", coursesRouter);
 app.use("/adminrouter",adminRouter)
 
 
-app.listen(3000,()=>{
-    console.log("app is running hot on the port 3000");
-})
+
+async function connectDB() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ridhiv:Samridhi2048@test.d6wd339.mongodb.net/?appName=test/backend-course-selling",
+    );
+    console.log("DB connected");
+    app.listen(3000, () => {
+      console.log("app is running hot on the port 3000");
+    });
+  } catch (err) {
+    console.log("DB error:", err);
+  }
+}
+connectDB();
