9d48d20df41671cddecf95ae2e252d790e7a37c6
ridhivsharma <ridhiv0305@gmail.com>
2026-03-08 14:24:42 +0530

    user sign p is estblished and bcrypt is used for the hashing of the thing that is password

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { userRouter } = require("./routes/user");
 const { coursesRouter } = require("./routes/courses");
 const {adminRouter} = require("./routes/adminRouter");
 const app = express();
-
+app.use(express.json());
 app.use("/user", userRouter);
 app.use("/courses", coursesRouter);
 app.use("/adminrouter",adminRouter)
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,45 @@
 const {Router}=require("express")
 const userRouter=Router();
-const userModel=require("../db")
+const bcrypt = require('bcrypt');
+const {userModel}=require("../db")
+
+const saltRounds=15;
 
 
 userRouter.post("/signup",(req,res)=>{
-    res.json({
-        "msg":"signup endpoint"    
+    let { email,password,firstName,lastName }=req.body;
+
+    bcrypt.hash(password, saltRounds)
+    .then(function(hash) {
+        password=hash;
+        
+        userModel.create({
+        email:email,
+        password:password,
+        firstName:firstName,
+        lastName:lastName
+        })
+        .then(()=>{
+            console.log("user has been created")
+            res.json({
+                "msg":"user has been created"
+                
+            })
+        })
+        .catch((e)=>{
+                console.log(e)
+                res.json({
+                    "msg":"some error has been occured"
+                })
+        })
     })
+    .catch((e)=>{
+        console.log(e);
+        return res.json({
+            "msg":"there has bene something error"
+        })
+    });
+    
 })
 
 userRouter.post("/signin",(req,res)=>{
